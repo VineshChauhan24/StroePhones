f8c5c7acd06c71bb0695432f236fa5167926d0ea
Ahmed Mahmoud <32569345+devahmedm@users.noreply.github.com>
2018-08-13 12:23:17 +0300

    Update MainActivity.java

--- Android Application/StorePhones/src/main/java/com/devahmedm/storephones/MainActivity.java
+++ Android Application/StorePhones/src/main/java/com/devahmedm/storephones/MainActivity.java
@@ -46,7 +46,7 @@ import io.realm.RealmResults;
 /**
  * Created by Dev Ahmed Mahmoud on 25/5/2018
  * email : dev.ahmed.m@gmail.com
- * phone : +9700597503338
+ * phone : +970597503338
  */
 
 public class MainActivity extends AppCompatActivity {
